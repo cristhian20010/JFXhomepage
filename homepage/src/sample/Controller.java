9e7d5079363bc7b6899909b0f1e8f2d60447c8c9
IvanZhen <ivan@codemain.es>
2017-11-17 10:53:09 +0100

    updated controller

--- homepage/src/sample/Controller.java
+++ homepage/src/sample/Controller.java
@@ -9,7 +9,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
+import java.awt.List;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class Controller  implements Initializable{
@@ -74,58 +77,46 @@ public class Controller  implements Initializable{
         fadeTransition.setFromValue(1);
         fadeTransition.setToValue(0);
         fadeTransition.play();
-
-        fadeTransition.setOnFinished(event -> {
+        
+        fadeTransition.setOnFinished(e -> {
             FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(3),pane3);
             fadeTransition1.setFromValue(1);
             fadeTransition1.setToValue(0);
             fadeTransition1.play();
-
-            fadeTransition1.setOnFinished(event1 -> {
-
+            
+            fadeTransition1.setOnFinished(e1 -> {
+            	
                 FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(3),pane2);
                 fadeTransition2.setFromValue(1);
                 fadeTransition2.setToValue(0);
                 fadeTransition2.play();
-
-                fadeTransition2.setOnFinished(event2 -> {
-
-                    FadeTransition fadeTransition00 = new FadeTransition(Duration.seconds(3),pane2);
-                    fadeTransition00.setFromValue(0);
-                    fadeTransition00.setToValue(1);
-                    fadeTransition00.play();
-
-
-                    fadeTransition00.setOnFinished(event3 -> {
-                        FadeTransition fadeTransition11 = new FadeTransition(Duration.seconds(3),pane3);
-                        fadeTransition11.setFromValue(0);
-                        fadeTransition11.setToValue(1);
-                        fadeTransition11.play();
-
-                        fadeTransition11.setOnFinished(event4 -> {
-                            FadeTransition fadeTransition22=new FadeTransition(Duration.seconds(3),pane4);
-                            fadeTransition22.setFromValue(0);
-                            fadeTransition22.setToValue(1);
-                            fadeTransition22.play();
-
-                            fadeTransition22.setOnFinished(event5 -> {
+                
+                fadeTransition2.setOnFinished(e2 -> {
+                	
+                    FadeTransition fadeTransition3 = new FadeTransition(Duration.seconds(3),pane2);
+                    fadeTransition3.setFromValue(0);
+                    fadeTransition3.setToValue(1);
+                    fadeTransition3.play();
+                    
+                    fadeTransition3.setOnFinished(e3 -> {
+                        FadeTransition fadeTransition4 = new FadeTransition(Duration.seconds(3),pane3);
+                        fadeTransition4.setFromValue(0);
+                        fadeTransition4.setToValue(1);
+                        fadeTransition4.play();
+                        
+                        fadeTransition4.setOnFinished(e4 -> {
+                            FadeTransition fadeTransition5 = new FadeTransition(Duration.seconds(3),pane4);
+                            fadeTransition5.setFromValue(0);
+                            fadeTransition5.setToValue(1);
+                            fadeTransition5.play();
+                            
+                            fadeTransition5.setOnFinished(e5 -> {
                                 Animation();
                             });
                         });
-
-
                     });
                 });
-
             });
-
-
-
-
         });
-
-
-
     }
-
 }
